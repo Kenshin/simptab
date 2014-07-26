4a690e843fed01794f70d918e6f012bb2d9c8499
Kenshin <fxblog@gmail.com>
2014-07-26 15:50:15 +0800

    add time logic

---
+++ js/date.js
@@ -0,0 +1,15 @@
+
+define( ["jquery"], function($){
+
+	return {
+		Show: function () {
+			var date = new Date();
+
+			$("#time").text(date.getHours() + ":" + date.getMinutes());
+			setInterval(function() {
+				date = new Date();
+				$("#time").text(date.getHours() + ":" + date.getMinutes());
+			},1000 * 30);
+		}
+	}
+});
--- js/main.js
+++ js/main.js
@@ -2,12 +2,18 @@
 requirejs.config({
     baseUrl: '.',
     paths: {
-      'jquery': 'vender/jquery-2.1.1.min',
-      'background': 'js/background'
+      'jquery'    : 'vender/jquery-2.1.1.min',
+      'background': 'js/background',
+      'date'      : 'js/date'
   	}
 });
 
 // main
-requirejs(['jquery', 'background' ],function ($, background) {
+requirejs(['jquery', 'background', 'date' ],function ($, background, date) {
+
+	// get background image
 	background.Get(false);
+
+	// get time
+	date.Show();
 });
