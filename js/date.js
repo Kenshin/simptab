ec1d02bcdc57640494816b62ffa2e7d8c8a0d897
Kenshin <fxblog@gmail.com>
2014-07-27 08:44:55 +0800

    add format minutes to date.js

--- js/date.js
+++ js/date.js
@@ -1,14 +1,21 @@
 
 define([ "jquery" ], function( $ ) {
 
+    formatMinutes = function ( value ) {
+        if ( value < 10 ) {
+        	return "0" + value;
+        }
+        return value;
+    }
+
     return {
         Show: function () {
             var date = new Date();
 
-            $( "#time" ).text( date.getHours() + ":" + date.getMinutes() );
+            $( "#time" ).text( date.getHours() + ":" + formatMinutes(date.getMinutes()) );
             setInterval(function() {
                 date = new Date();
-                $( "#time" ).text(date.getHours() + ":" + date.getMinutes() );
+                $( "#time" ).text(date.getHours() + ":" + formatMinutes(date.getMinutes()) );
             }, 1000 * 30 );
         }
     }
