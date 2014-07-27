275b5dbdbe0f9b2b15ba60c2937ceaf8803e2a36
Kenshin <fxblog@gmail.com>
2014-07-27 14:12:47 +0800

    add download background logic

--- js/background.js
+++ js/background.js
@@ -7,21 +7,31 @@ define([ "jquery" ], function( $ ) {
             timeout    : 2000,
             url        : "http://bing.com",
             success    : function( data ) {
-                //console.log(data);
-                var begin = data.indexOf( "g_img=" ),
-                       newdata = data.substr(begin),
-                       end   = newdata.indexOf( ".jpg" ),
-                       url   = newdata.substring( 12, end ) + ".jpg";
+                console.log(data)
+                var begin   = data.indexOf( "g_img=" ),
+                    newdata = data.substr( begin ),
+                    end     = newdata.indexOf( ".jpg" ),
+                    url     = newdata.substring( 12, end ) + ".jpg",
+                    hdurl   = getHDurl( url );
 
-                console.log("url = " + url);
-                console.log("end = " + end);
+                console.log("url = "   + hdurl);
+                console.log("end = "   + end);
                 console.log("begin = " + begin);
 
                 // set background image
-                $("body").css({ "background-image": "url(" + get1080p( url ) + ")" });
+                $("body").css({ "background-image": "url(" + hdurl + ")" });
 
-                // save
-                save( url );
+                // transfor to datauri
+                save( hdurl );
+
+                // get background name
+                begin    = data.indexOf( 'id="sh_cp" title="' );
+                newdata  = data.substr( begin + 18 );
+                end      = newdata.indexOf( '"' );
+                var name = newdata.substring( 0, end ) + ".jpg";
+
+                // download
+                download( hdurl, name );
             }
         });
     }
@@ -30,22 +40,37 @@ define([ "jquery" ], function( $ ) {
         console.log("getRandom")
     }
 
-    get1080p = function ( url ) {
+    getHDurl = function ( url ) {
         return url.replace( "1366x768", "1920x1080" );
     }
 
     save = function ( url ) {
         var img = new Image();
         img.onload = function() {
+
+            // set canvas
             var canvas = document.createElement( "canvas" );
             canvas.width = img.width;
             canvas.height = img.height;
             var ctx = canvas.getContext( "2d" );
             ctx.drawImage( img, 0, 0 );
+
+            // get datauri
             var dataURI = canvas.toDataURL();
+
+            // set chrome local storage
             chrome.storage.local.set({ background : dataURI });
+
         }
         img.src = url;
+    }
+
+    download = function( url, name ) {
+        // set download href
+        $( ".controlink[url='download']" ).attr({
+            'href'      : url,
+            'download'  : name + '.jpg'
+        });
     }
 
     return {
@@ -53,7 +78,7 @@ define([ "jquery" ], function( $ ) {
             var url = "../assets/images/background.jpg";
             chrome.storage.local.get( "background", function( result ) {
                 if ( result && !$.isEmptyObject( result )) {
-                    url = result.background;
+                    //url = result.background;
                 } else {
                     // get background
                     if ( is_random ) {
@@ -65,6 +90,8 @@ define([ "jquery" ], function( $ ) {
                 }
                 // set background
                 $("body").css({ "background-image": "url(" + url + ")" });
+                // temp
+                getDefault();
             });
         }
     }
--- js/controlbar.js
+++ js/controlbar.js
@@ -3,9 +3,18 @@ define([ "jquery" ], function( $ ) {
 
     return {
         Listen: function () {
-            // listen control bar
+
+            // listen chrome link
             $( ".chromelink" ).click( function( event ) {
                 chrome.tabs.create({ url : $( event.currentTarget ).attr( "url" ) });
+            });
+
+            // listen control link
+            $( ".controlink" ).click( function( event ) {
+                $target = $( event.currentTarget ).attr( "url" );
+                if ( $target == "setting" ) {
+                    //TO DO
+                }
             });
         }
     }
