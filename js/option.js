2670ee6923c74ec003e28f9ffc44a883d897f48b
Kenshin <fxblog@gmail.com>
2014-07-31 16:55:59 +0800

    change radio input style

--- js/option.js
+++ js/option.js
@@ -1,6 +1,25 @@
 
 define([ "jquery", "date" ], function( $, date ) {
 
+    changeRadioState = function( $target ) {
+        var $current = $target.parent(),
+            $prev    = $current.prev(),
+            $next    = $current.next();
+
+        if ( $prev.find( "span" ).length > 0 ) {
+            $prev.find( "span" ).removeClass( "checked" );
+            $prev.find( "span" ).addClass( "unchecked" );
+            $current.find( "span" ).removeClass( "unchecked" );
+            $current.find( "span" ).addClass( "checked" );
+        }
+        else {
+            $next.find( "span" ).removeClass( "checked" );
+            $next.find( "span" ).addClass( "unchecked" );
+            $current.find( "span" ).removeClass( "unchecked" );
+            $current.find( "span" ).addClass( "checked" );
+        }
+    }
+
     return {
         Init: function() {
 
@@ -8,12 +27,14 @@ define([ "jquery", "date" ], function( $, date ) {
             if ( mode != undefined ) {
                 $( ".changestate input[value=" +  mode + "]" ).attr( "checked", true );
                 $( ".changestate input[value!=" + mode + "]" ).attr( "checked", false );
+                changeRadioState( $( ".changestate input[value=" + mode + "]" ) );
             }
 
             mode = localStorage["simptab-background-clock"];
             if ( mode != undefined ) {
                 $( ".clockstate input[value=" +  mode + "]" ).attr( "checked", true );
                 $( ".clockstate input[value!=" + mode + "]" ).attr( "checked", false );
+                changeRadioState( $( ".clockstate input[value=" + mode + "]" ) );
             }
         },
 
@@ -21,10 +42,12 @@ define([ "jquery", "date" ], function( $, date ) {
 
             $( ".changestate input" ).click( function( event ) {
                 localStorage["simptab-background-mode"] = $(event.currentTarget).attr( "value" );
+                changeRadioState( $( event.currentTarget ));
             });
 
             $( ".clockstate input" ).click( function( event ) {
                 localStorage["simptab-background-clock"] = $(event.currentTarget).attr( "value" );
+                changeRadioState( $( event.currentTarget ));
                 if ( localStorage["simptab-background-clock"] == "show") {
                     date.Show();
                 }
