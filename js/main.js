cfdb16a1c43ec6cd789c9fd49b70e39aa3dc391e
Kenshin <fxblog@gmail.com>
2014-08-14 12:02:56 +0800

    add zh-cn font and logic

--- js/main.js
+++ js/main.js
@@ -43,8 +43,24 @@ $( document ).ready( function() {
     // i18n init
     i18n.init( options, function( t ) {
 
-    	$(".title > h1 > div").html( t( "title" ));
-    	$(".download > .btn-download > div").html( t( "download" ));
+    	$(".title h1 div").html( t( "title" ));
+    	$(".download .btn-download div").html( t( "download" ));
+        $(".download .smaller").html( t( "or" ));
+        $(".download .btn-offline-download").html( t( "offline" ));
+
+        $(".main li:nth-of-type(1) span").html( t( "feature1" ));
+        $(".main li:nth-of-type(2) span").html( t( "feature2" ));
+        $(".main li:nth-of-type(3) span").html( t( "feature3" ));
+
+        $(".footer li:nth-child(1) h2").html( t( "support" ));
+        $(".footer li:nth-child(1) div span:nth-of-type(1)").html( t( "feedback" ));
+        $(".footer li:nth-child(1) div span:nth-of-type(2)").html( t( "contact" ));
+        $(".footer li:nth-child(1) div a:nth-of-type(2)").html( t( "author" ).toLowerCase() );
+
+        $(".footer li:nth-child(2) h2").html( t( "author" ));
+        $(".footer li:nth-child(2) p:nth-child(2)").html( t( "job" ));
+
+        $(".footer .copyright span:nth-child(1)").html( t( "footer" ));
     });
 
 });
