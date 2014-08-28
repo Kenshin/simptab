5f8b42e82febb6837d1347fbade87ee0c246d083
kenshin <fxblog@gmail.com>
2014-08-28 12:18:50 +0800

    Optimize shortcuts framework.

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -2,18 +2,38 @@
 define([ "jquery", "mousetrap", "controlbar" ], function( $, Mousetrap, controlbar ) {
 
     var CONTROL_KEY_MAP = [
-        "b o o k",
-        "h i s",
-        "a p p",
-        "i n f o",
-        "d o w n",
-        "s e t"
+        "book",
+        "his",
+        "app",
+        "info",
+        "down",
+        "set"
     ];
+
+    formatShortcut = function( key ) {
+
+        var formatter = "",
+            arr       = new Array( key.length );
+
+        $.each( arr, function ( idx, value ) {
+
+            if ( idx < key.length - 1 ) {
+                formatter += key[idx] + " ";
+            }
+            else {
+                formatter += key[idx];
+            }
+
+        });
+
+        return formatter;
+    }
 
     listenControl = function () {
 
         $.each( CONTROL_KEY_MAP, function( idx, shortcut ) {
-            Mousetrap.bind( shortcut, function() {
+            var new_key = formatShortcut( shortcut );
+            Mousetrap.bind( new_key , function() {
                 console.log("click = " + shortcut.replace( / /g, "" ) );
                 controlbar.AutoClick( idx );
             });
