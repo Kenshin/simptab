0e2c7244f1c7487125fbabe877faec42da9c15a7
Kenshin <fxblog@gmail.com>
2015-04-06 18:21:20 +0800

    Add apis.holiday().

--- js/apis.js
+++ js/apis.js
@@ -463,6 +463,33 @@ define([ "jquery", "i18n", "setting", "vo", "date" ], function( $, i18n, setting
     }
 
     /*
+    * Holiday background
+    */
+
+    const HOLIDAY_LIST = [20150204, 20150219, 20150306, 20150321, 20150405, 20150420, 20150506, 20150521, 20150606, 20150622, 20150707, 20150723, 20150808, 20150823, 20150908, 20150923, 20151008, 20151024, 20151108, 20151122, 20151207, 20151222, 20150106, 20150120];
+
+    isHoliday = function() {
+        var new_holiday = date.Today(),
+            old_holiday = localStorage["simptab-holiday"];
+
+        // add test code
+        new_holiday = 20150420;
+
+        if ( HOLIDAY_LIST.filter(function(item){return item == new_holiday;}).length > 0 && old_holiday != new_holiday ) {
+            localStorage["simptab-holiday"] = new_holiday;
+            localStorage["simptab-background-refresh"] = "true";
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    holiday = function() {
+        special( "holiday" );
+    }
+
+    /*
     * Special day/Holiday background
     */
     special = function() {
@@ -478,27 +505,29 @@ define([ "jquery", "i18n", "setting", "vo", "date" ], function( $, i18n, setting
         .done( function( result ) {
             if ( result && !$.isEmptyObject( result )) {
                 var obj = result[type],
-                    key;
+                    key, max, random, special_day, data, hdurl;
 
                 if ( type == "special" ) {
-
-                    key = obj.now.length > 0 ? "now" : "old";
-
-                    var max         = obj[key].length - 1,
-                        random      = createRandom( 0, max ),
-                        special_day = obj[key][random],
-                        data, hdurl;
-
-                    data   = special_day.day;
-                    max    = data.hdurl.length - 1;
-                    random = createRandom( 0, max );
-                    hdurl  = query_host + data.key + "/" + data.hdurl[random] + ".jpg";
-
-                    deferred.resolve( vo.Create( hdurl, hdurl, data.name, data.info, date.Now(), data.name, "special" ));
+                    key         = obj.now.length > 0 ? "now" : "old";
+                    max         = obj[key].length - 1;
+                    random      = createRandom( 0, max );
+                    special_day = obj[key][random];
+                    data        = special_day.day;
+                    max         = data.hdurl.length - 1;
+                    random      = createRandom( 0, max );
+                    hdurl       = query_host + data.key + "/" + data.hdurl[random] + ".jpg";
                 }
                 else {
 
+                    key         = date.Today();
+                    // add test code
+                    key         = 20150420;
+                    data        = obj[key];
+                    max         = data.hdurl.length - 1;
+                    random      = createRandom( 0, max );
+                    hdurl       = query_host + type + "/" + data.hdurl[random] + ".jpg";
                 }
+                deferred.resolve( vo.Create( hdurl, hdurl, data.name, data.info, date.Now(), data.name, type ));
             }
             else {
                 deferred.reject( null, "Not found any special day/Holiday backgroudn from " + query_host + SPECIAL_URL, null )
@@ -513,12 +542,15 @@ define([ "jquery", "i18n", "setting", "vo", "date" ], function( $, i18n, setting
 
       Init: function () {
 
-        const MAX_NUM = 8;
+        const MAX_NUM = 9;
 
         var code = createRandom( 0, MAX_NUM );
 
+        if ( isHoliday() ) {
+            code = 8;
+        }
         // check setting is random, when not random must call bing.com, so random is 4
-        if ( !setting.IsRandom() ) {
+        else if ( !setting.IsRandom() ) {
           code = MAX_NUM;
         }
         else {
@@ -559,6 +591,9 @@ define([ "jquery", "i18n", "setting", "vo", "date" ], function( $, i18n, setting
             break;
           case 7:
             special();
+            break;
+          case 8:
+            holiday();
             break;
           default:
             bing();
