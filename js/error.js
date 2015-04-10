d492ca18a2d19c34d43ee96a4dbca225c4548a1d
Kenshin <fxblog@gmail.com>
2015-04-10 16:40:34 +0800

    Add error.js 'use strict'.

--- js/error.js
+++ js/error.js
@@ -1,5 +1,7 @@
 
-define([ "jquery", "i18n", "vo", "date", "files" ], function( $, i18n, vo, date, files ) {
+define([ "jquery" ], function( $ ) {
+
+	"use strict";
 
 	function SimpError( method_name, message, data ) {
 		Error.apply( this, arguments );
