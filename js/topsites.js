b0f9f05373485ed792d9c7ce98297f340668a1d7
Kenshin <fxblog@gmail.com>
2015-04-14 13:51:52 +0800

    Add topSites UI.

--- js/topsites.js
+++ js/topsites.js
@@ -3,6 +3,20 @@ define([ "jquery" ], function( $ ) {
 
     function topSitesRender( sites ) {
         console.log( "Topsites", sites );
+        var $item = $('<li><a href="#" class="siteurl"><span class="sitetitle"></span></a></li>' ),
+            $a    = $( $item.find("a")    ),
+            $span = $( $item.find("span") ),
+            len   = sites && sites.length > 10 ? 10 : sites.length,
+            item  = "", site;
+        for( var i = 0; i < len; i++ ) {
+            site = sites[i];
+
+            $a.attr( "href", site.url );
+            $span.text( site.title );
+
+            item += $item[0].outerHTML;
+        }
+        $(".topsites").append( item );
     }
 
     return {
