d0e84cdce085acd760b45354ddfec73f7184bf21
Kenshin <fxblog@gmail.com>
2015-04-16 13:48:20 +0800

    Add omnibox logic.

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -1,5 +1,5 @@
 
-define([ "jquery", "mousetrap", "controlbar" ], function( $, Mousetrap, controlbar ) {
+define([ "jquery", "mousetrap", "controlbar", "i18n" ], function( $, Mousetrap, controlbar, i18n ) {
 
     "use strict";
 
@@ -59,10 +59,39 @@ define([ "jquery", "mousetrap", "controlbar" ], function( $, Mousetrap, controlb
         });
     }
 
+    function listenOminbox() {
+
+        chrome.omnibox.setDefaultSuggestion({ description : "输入空格显示全部的命令列表，目前支持的关键字包括：book, his, app, info, down, up, set, fav" });
+
+        chrome.omnibox.onInputChanged.addListener( function( command, suggest ) {
+            console.log( "SimpTab command is " + command );
+
+            var suggestResult =[];
+            if ( command.trim() === "" ) {
+                for( var i = 0, len = CONTROL_KEY_MAP.length; i < len; i++ ) {
+                    suggestResult.push( { content : CONTROL_KEY_MAP[i], description : CONTROL_KEY_MAP[i] } );
+                }
+                suggest( suggestResult );
+            }
+          });
+
+        chrome.omnibox.onInputEntered.addListener( function( command ) {
+            console.log( "SimpTab command is " + command );
+            var idx = CONTROL_KEY_MAP.indexOf( command );
+            if ( idx > -1 ) {
+                controlbar.AutoClick( idx );
+            }
+            else {
+
+            }
+        });
+    }
+
     return {
         Init: function () {
             listenControl();
             listenCommand();
+            listenOminbox();
         }
     };
 });
