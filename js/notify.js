53eb713a98056d63d5efea9e3ae73936d0c16e7c
Kenshin <fxblog@gmail.com>
2015-04-22 11:17:48 +0800

    Add close event effect.

--- js/notify.js
+++ js/notify.js
@@ -42,8 +42,9 @@ define([ "jquery" ], function( $ ) {
             <div class="content">New version changlog here.</div>\
         </div>',
         closeHandle = function( event ) {
+            var self = $(this).parent();
             $container.undelegate( "." + event.data + " .close", "click", closeHandle );
-            $(this).parent().remove();
+            self.hide( 500, function() { self.remove(); });
         },
         render = function() {
             var $tmpl    = $( TMPL ),
