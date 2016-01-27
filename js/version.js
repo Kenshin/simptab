41b94327020e9ded8cc0c1df9413ace5ad4e7e18
Kenshin <fxblog@gmail.com>
2016-01-27 15:15:00 +0800

    Add version removePermissions.

--- js/version.js
+++ js/version.js
@@ -40,7 +40,6 @@ define([ "jquery", "notify", "i18n" ], function( $, Notify, i18n ) {
                     "http://*.wallhaven.cc/",
                     "https://*.staticflickr.com/",
                     "http://*.desktopprassets.com/",
-                    "http://*.visualhunt.com/",
                     "https://*.500px.org/",
                     "http://*.vo.msecnd.net/",
                     "http://*.nasa.gov/"
