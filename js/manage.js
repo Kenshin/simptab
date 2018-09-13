ca19c193c2674e1888fb9a8cc26cfbcc37ddf094
Kenshin <fxblog@gmail.com>
2018-09-13 16:19:16 +0800

    Add manage tab UI.

--- js/manage.js
+++ js/manage.js
@@ -3,11 +3,20 @@ define([ "jquery", "lodash", "i18n", "vo", "date", "error" ], function( $, _, i1
 
     "use strict";
 
+    var tmpl = '<div class="tabs">\
+                    <div class="tab tab-active">收藏</div>\
+                    <div class="tab">订阅</div>\
+                </div>\
+                <div class="album"></div>\
+               ';
+
     return {
         Render: function () {
+            var compiled = _.template( tmpl );
             $( "body" ).append( '<div class="manage-overlay"><div class="manage-bg"><div class="manage"></div></div></div>' );
             setTimeout( function() {
                 $( ".manage-bg" ).addClass( "manage-bg-show" );
+                $( ".manage" ).html( compiled );
             }, 10 );
         }
     };
