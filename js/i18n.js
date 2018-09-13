ade1965a4fe5b09eb833849392971426af5bf07b
Kenshin <fxblog@gmail.com>
2018-09-13 14:57:14 +0800

    Add manage i18n.

--- js/i18n.js
+++ js/i18n.js
@@ -16,6 +16,7 @@ define([ "jquery" ], function( $ ) {
         $($( ".controlbar" ).children()[5]).find( "a[data-balloon]" ).attr("data-balloon", i18n( "controlbar_upload" ));
         $($( ".controlbar" ).children()[6]).find( "a[data-balloon]" ).attr("data-balloon", i18n( "controlbar_refresh" ));
         $($( ".controlbar" ).children()[7]).find( "a[data-balloon]" ).attr("data-balloon", i18n( "controlbar_setting" ));
+        $( ".controlbar" ).find( "a[url=manage]" ).attr("data-balloon", i18n( "controlbar_manage" ));
         $($( ".controlbar" ).children()[8]).find( "a[data-balloon]" ).attr("data-balloon", i18n( "controlbar_favorite" ));
         $($( ".controlbar" ).children()[9]).find( "a[data-balloon]" ).attr("data-balloon", i18n( "controlbar_pin" ));
         $($( ".controlbar" ).children()[10]).find( "a[data-balloon]" ).attr("data-balloon", i18n( "controlbar_dislike" ));
