de66a5803f110b1ab65d1e72962c00d0eb0fb259
Kenshin <fxblog@gmail.com>
2018-09-19 15:51:30 +0800

    Add about page.

--- js/about.js
+++ js/about.js
@@ -1,8 +1,27 @@
 define([ "jquery", "lodash", "notify", "i18n" ], function( $, _, Notify, i18n ) {
 
+    var rTmpl  = '\
+                <div class="close"><span class="close"></span></div>\
+                    <div class="about">About</div>\
+                </div>';
+
+    function closeListenEvent() {
+        $( ".manage .close" ).click( function( event ) {
+            $( ".manage-bg" ).removeClass( "manage-bg-show" );
+            setTimeout( function() {
+                $( ".manage-overlay" ).remove();
+            }, 400 );
+        });
+    }
+
     return {
         Render: function() {
-            // TO-DO
+            $( "body" ).append( '<div class="manage-overlay"><div class="manage-bg"><div class="manage"></div></div></div>' );
+            setTimeout( function() {
+                $( ".manage-bg" ).addClass( "manage-bg-show" );
+                $( ".manage" ).html( rTmpl );
+                closeListenEvent();
+            }, 10 );
         }
     }
 });
