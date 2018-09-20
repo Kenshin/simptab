8f09bc14c3bcd181284382b8b625d066ae504e33
Kenshin <fxblog@gmail.com>
2018-09-20 12:26:43 +0800

    Update about page.

--- js/about.js
+++ js/about.js
@@ -8,10 +8,10 @@ define([ "jquery", "lodash", "notify", "i18n" ], function( $, _, Notify, i18n ) 
                             <h1>简 Tab</h1>\
                             <h3>' + i18n.GetLang( 'extension_desc' ) + '去除多余功能，只关注标签页呈现效果。</h3>\
                             <p className="desc">\
-                                嗨，感谢使用 简 Tab，希望它可以给你一成不变的 New Tab 带来一些不同；<br>\
+                                嗨，感谢使用 简 Tab，希望它可以给你一成不变的 New Tab 带来一些不同； 😊 <br>\
                                 简 Tab 只关注如何能更好的在 New Tab 呈现背景，这些背景均来自于：<code>bing.com</code> · <code>unsplash.com</code> · <code>flickr.com</code> · <code>wallhaven.cc</code> · <code>googleartproject.com</code> · <code>desktoppr.co</code> · <code>visualhunt.com</code> <br>\
                                 同时与其它同类扩展最大的不同是，简 Tab 拥有自己的 <code>精选集</code>；如同 iPhone 上的壁纸类 App 一样，通过 <code>人工筛选</code> 的方式进一步将适合作为 New Tab 背景的壁纸呈现给你。<br><br>\
-                                如果你有好的作品 亦或 适合的背景，也请告诉 简 Tab，投稿地址 👉 <a href="" target="_blank">猛戳这里</a> · <a href="" target="_blank">备用地址</a> <br>\
+                                如果你有 <code>好的作品</code> 亦或 <code>适合的背景</code>，也请告诉 简 Tab，投稿地址 👉 <a href="" target="_blank">猛戳这里</a> · <a href="" target="_blank">备用地址</a> <br>\
                             </p>\
                             \
                         </div>\
