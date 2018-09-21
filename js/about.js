2de6503fc1ccf2a1f4c2eaf2f0646bba1d22bcdb
Kenshin <fxblog@gmail.com>
2018-09-21 15:45:10 +0800

    Chanage mange to dialog.

--- js/about.js
+++ js/about.js
@@ -27,9 +27,9 @@ define([ "jquery", "lodash", "notify", "i18n" ], function( $, _, Notify, i18n ) 
 
     function closeListenEvent() {
         $( ".manage .close" ).click( function( event ) {
-            $( ".manage-bg" ).removeClass( "manage-bg-show" );
+            $( ".dialog-bg" ).removeClass( "dialog-bg-show" );
             setTimeout( function() {
-                $( ".manage-overlay" ).remove();
+                $( ".dialog-overlay" ).remove();
             }, 400 );
         });
     }
@@ -43,9 +43,9 @@ define([ "jquery", "lodash", "notify", "i18n" ], function( $, _, Notify, i18n ) 
 
     return {
         Render: function() {
-            $( "body" ).append( '<div class="manage-overlay"><div class="manage-bg"><div class="manage" style="height:auto;"></div></div></div>' );
+            $( "body" ).append( '<div class="dialog-overlay"><div class="dialog-bg"><div class="manage" style="height:auto;"></div></div></div>' );
             setTimeout( function() {
-                $( ".manage-bg" ).addClass( "manage-bg-show" );
+                $( ".dialog-bg" ).addClass( "dialog-bg-show" );
                 $( ".manage" ).html( rTmpl );
                 closeListenEvent();
                 moreAbout();
