07363fcce67523dcf18c0d15ca8d0f9701efceec
Kenshin <fxblog@gmail.com>
2018-09-22 11:41:56 +0800

    Optimize setting and controlbar css.

--- js/controlbar.js
+++ js/controlbar.js
@@ -135,6 +135,7 @@ define([ "jquery", "i18n", "vo", "date", "files", "setting", "manage", "about" ]
                             $( ".setting" ).animate({ "width": width, opacity : 0.8 }, 500, function() {
                                 $target.addClass( "close" );
                             });
+                            $( ".clock" ).animate({ "right": width + 10 }, 500 );
 
                             $( ".seniorgp, .bottom" ).animate({ right: parseInt($(".bottom").css("right")) + width }, 500 ); // 116-simptab-optimize-layout
 
@@ -153,6 +154,7 @@ define([ "jquery", "i18n", "vo", "date", "files", "setting", "manage", "about" ]
                             $( ".setting" ).animate({ width: 0, opacity : 0 }, 500, function() {
                                 $target.removeClass( "close" );
                             });
+                            $( ".clock" ).animate({ "right": 10 }, 500 );
                             $( ".seniorgp, .bottom" ).animate({ right: "65px" }, 500 );    // 116-simptab-optimize-layout
                         }
                         break;
