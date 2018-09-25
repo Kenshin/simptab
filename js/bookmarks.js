20af4cdac3138a63f3e71d856db8e9c7aa8a61f0
Kenshin <fxblog@gmail.com>
2018-09-25 12:30:52 +0800

    Add bookmarks global event listen.

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,8 +1,20 @@
 define([ "jquery", "waves", "i18n" ], function( $, Waves, i18n ) {
 
+    function bmListen() {
+        $( ".bm-overlay" ).mouseenter( function() {
+            $( ".bm" ).css({ "transform": "translateX(0px)", "opacity": 1 });
+        });
+        $( ".bm" ).mouseleave( function() {
+            $( ".bm" ).css({ "transform": "translateX(-300px)", "opacity": 0 });
+        });
+    }
+
     return {
         Render() {
-            
+            $( "body" ).append( '<div class="bm-overlay"><div class="bm"></div></div>' );
+            setTimeout( function() {
+                bmListen();
+            }, 10 );
         }
     }
 });
