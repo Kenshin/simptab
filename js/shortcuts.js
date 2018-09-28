c721874a9b4b9c9096fd0ac73a83ade31c7c0e05
Kenshin <fxblog@gmail.com>
2018-09-28 10:34:51 +0800

    Optimize shortcuts layout.

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -195,7 +195,9 @@ define([ "jquery", "mousetrap", "controlbar", "i18n", "topsites", "message" ], f
             if ( $( ".shortcuts" ).children().length > 0 ) {
                 $( ".shortcuts" ).remove();
             } else {
-                var html = createKeymapTmpl( "控制栏快捷键", keys.CONTROL_KEY_MAP, "controlbar_" );
+                var html = createKeymapTmpl( "全局快捷键", keys.GLOBALS_KEY_MAP, "controlbar_" );
+                html    += createKeymapTmpl( "其它快捷键", keys.OTHERS_KEY_MAP, "controlbar_" );
+                html    += createKeymapTmpl( "控制栏快捷键", keys.CONTROL_KEY_MAP, "controlbar_" );
                 $( "body" ).append( '<div class="shortcuts"><div class="title">快捷键一览</div><div class="keymaps">' + html + '</div></div>' );
             }
         });
