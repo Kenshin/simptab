d09b530c062ffc563898cf8f074a3e4058707692
Kenshin <fxblog@gmail.com>
2018-09-28 15:39:42 +0800

    Add open/close logic and effect.

--- js/welcome.js
+++ js/welcome.js
@@ -1,6 +1,30 @@
 
 define([ "jquery", "mousetrap", "carousel", "i18n" ], function( $, Mousetrap, carousel, i18n ) {
 
+    function open() {
+        $( ".welcome" ).css({ "transform": "translateY(0px)", "opacity": 1 });
+    }
+
+    function close() {
+        $( ".welcome" ).css({ "transform": "translateY(100px)", "opacity": 0 });
+        setTimeout( function() {
+            $( ".welcome-overlay" ).remove();
+        }, 500 );
+    }
+
+    function carousel() {
+        $('.carousel.carousel-slider').carousel({
+            fullWidth: true,
+            indicators: true
+        });
+    }
+
+    function listen() {
+        $( ".welcome .close" ).on( "click", function() {
+            close();
+        });
+    }
+
     return {
         Render: function() {
             var tmpl = '<div class="carousel-item">\
@@ -15,13 +39,12 @@ define([ "jquery", "mousetrap", "carousel", "i18n" ], function( $, Mousetrap, ca
                         <div class="carousel-item">\
                             <img src="http://ojec5ddd5.bkt.clouddn.com/welcome-sites.png">\
                         </div>';
-            $( "body" ).append( '<div class="welcome-overlay"><div class="welcome carousel carousel-slider">' + tmpl + '</div></div>' )
+            $( "body" ).append( '<div class="welcome-overlay"><div class="welcome carousel carousel-slider"><div class="close"><span class="close"></span></div>' + tmpl + '</div></div>' )
             setTimeout( function() {
-                $('.carousel.carousel-slider').carousel({
-                    fullWidth: true,
-                    indicators: true
-                  });
-            });
+                open();
+                carousel();
+                listen();
+            }, 10 );
         }
     }
 });
