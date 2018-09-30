b109be544421a61bcb531f58f5fe9dcd3ed5b20d
Kenshin <fxblog@gmail.com>
2018-09-30 13:57:58 +0800

    Add paging UI.

--- js/welcome.js
+++ js/welcome.js
@@ -10,7 +10,16 @@ define([ "jquery", "mousetrap", "carousel", "i18n" ], function( $, Mousetrap, ca
                 <div class="carousel-item" id="1.5.2">\
                     <img src="http://ojec5ddd5.bkt.clouddn.com/welcome-plugins.png">\
                 </div>',
-    }
+    },
+    paging = '\
+            <div class="paging">\
+                <a class="next waves-effect waves-circle" href="#">\
+                    <span>→</span>\
+                </a>\
+                <a class="prev waves-effect waves-circle" href="#">\
+                    <span>←</span>\
+                </a>\
+            </div>';
 
     function getDetails( ver ) {
         var detail = "";
@@ -55,7 +64,7 @@ define([ "jquery", "mousetrap", "carousel", "i18n" ], function( $, Mousetrap, ca
                         <div class="carousel-item" id="end">\
                             <img src="http://ojec5ddd5.bkt.clouddn.com/welcome-sites.png">\
                         </div>';
-            $( "body" ).append( '<div class="welcome-overlay"><div class="welcome carousel carousel-slider"><div class="close"><span class="close"></span></div>' + tmpl + '</div></div>' )
+            $( "body" ).append( '<div class="welcome-overlay">' + paging + '<div class="welcome carousel carousel-slider"><div class="close"><span class="close"></span></div>' + tmpl + '</div></div>' )
             setTimeout( function() {
                 open();
                 carousel();
