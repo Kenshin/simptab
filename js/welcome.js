69733d6efc89e4db6cb2c9806cad476caf3d60e1
Kenshin <fxblog@gmail.com>
2018-10-06 12:08:31 +0800

    Optimize welcome page layout.

--- js/welcome.js
+++ js/welcome.js
@@ -5,10 +5,18 @@ define([ "jquery", "mousetrap", "lodash", "carousel", "i18n" ], function( $, Mou
             "1.5.1": '\
                     <div class="carousel-item" id="1.5.1">\
                         <img src="http://ojec5ddd5.bkt.clouddn.com/welcome-adapter.png">\
+                        <div class="content">\
+                            <h2 class="title"></h2>\
+                            <div class="desc"></div>\
+                        </div>\
                     </div>',
             "1.5.2": '\
                     <div class="carousel-item" id="1.5.2">\
                         <img src="http://ojec5ddd5.bkt.clouddn.com/welcome-plugins.png">\
+                        <div class="content">\
+                            <h2 class="title"></h2>\
+                            <div class="desc"></div>\
+                        </div>\
                     </div>',
         },
         welcomeTmpl = '\
@@ -70,10 +78,18 @@ define([ "jquery", "mousetrap", "lodash", "carousel", "i18n" ], function( $, Mou
         Render: function( ver ) {
             var tmpl = '<div class="carousel-item" id="start">\
                             <img src="http://ojec5ddd5.bkt.clouddn.com/welcome-service-v2.png">\
+                            <div class="content">\
+                                <h2 class="title">欢迎使用</h2>\
+                                <div class="desc">' + ( ver.first ? "嗨，很高兴认识你，望你每次打开 New Tab 都有好心情。" : "您的 简 Tab 已焕然一新" ) + '</div>\
+                            </div>\
                         </div>\
                        ' + getDetails( ver ) + '\
                         <div class="carousel-item" id="end">\
                             <img src="http://ojec5ddd5.bkt.clouddn.com/welcome-sites.png">\
+                            <div class="content">\
+                                <h2 class="title"></h2>\
+                                <div class="desc">极简的 Chrome 新标签页扩展，望你每次打开都有好心情。</div>\
+                            </div>\
                         </div>';
             var compiled = _.template( welcomeTmpl ),
                 html     = compiled({ prev: prev_paging, next: next_paging, welcome: tmpl });
