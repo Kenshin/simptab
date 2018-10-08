339b6cbe2e65df05d26efad49d30e608ab69f808
Kenshin <fxblog@gmail.com>
2018-10-08 13:43:23 +0800

    Add test code.

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ requirejs([ "jquery", "lodash", "notify", "background", "date" , "controlbar", "
     version.Init( function( ver ) {
         welcome.Render( ver );
     });
-    welcome.Render({ first: true, update: "1.5.1" });
+    welcome.Render({ first: true, update: "1.5.2" });
 
     // waves config
     Waves.attach( '.icon',      [ 'waves-circle']);
