bfd0f33d471f56e7ccac94650953cb961c7e972d
Kenshin <fxblog@gmail.com>
2018-11-06 13:43:27 +0800

    Add zen mode style and logic.

--- js/zen.js
+++ js/zen.js
@@ -3,9 +3,42 @@ define([ "jquery", "lodash", "notify", "i18n", "vo", "date" ], function( $, _, N
 
     "use strict";
 
+    function timeMode() {
+        $( ".clock" ).addClass( "clock-bg-zen-mode" ).addClass( "zen-mode-bg" ).css( "z-index", "initial" );
+        $( "#time" ).addClass( "time-zen-mode" );
+    }
+
+    function dayMode() {
+        var date = new Date(),
+            day  = date.toLocaleDateString( navigator.language, { weekday: 'long', month: 'long', day: 'numeric' });
+        $( "#time" ).after( '<div class="day-zen-mode">' + day + '</div>' );
+    }
+
+    function devicesMode() {
+        navigator.getBattery().then( function( battery ) {
+            var network = navigator.onLine ? '≈ ' + navigator.connection.downlink + ' Mbps' : 'Offline',
+                charge  = ( battery.level * 100 ).toFixed() + '% ' + ( battery.charging ? 'Charging' : 'Battery' );
+            $( ".day-zen-mode" ).after( '<div class="devices-zen-mode">' + network + ' · ' + charge + '</div>' );
+        });
+    }
+
+    function topSitesMode() {
+        $( ".bottom" ).addClass( "bottom-zen-mode" );
+        $( ".topsites" ).addClass( "topsites-zen-mode" );
+        $( ".topsites a" ).addClass( "topsites-a-zen-mode" );
+    }
+
     return {
         Render: function() {
-            
+            timeMode();
+            dayMode();
+            devicesMode();
+
+            setTimeout( function() {
+                $( "body" ).removeClass( "bgmask" ).removeAttr( "style" );
+                $( ".bgmask-bg" ).remove();
+                topSitesMode();
+            }, 500 );
         },
 
         Exit: function() {
