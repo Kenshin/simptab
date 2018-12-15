0f8e73ddf2ac0e2b5064e5f363e67f490bc68062
Kenshin <fxblog@gmail.com>
2018-12-15 17:05:37 +0800

    Add sfx UI/Layout.

--- js/noise.js
+++ js/noise.js
@@ -54,7 +54,31 @@ define([ "jquery", "mousetrap", "lodash", "notify", "i18n", "comps", "message" ]
                             <span class="mode cafe001 active">安静的咖啡馆</span>\
                             <span class="mode">露天咖啡馆</span>\
                             <span class="mode">酒吧</span>\
-                            <span class="mode">嘈杂的咖啡馆</span>\
+                            <span class="mode">更多场景</span>\
+                        </div>\
+                        <div class="sfx">\
+                            <span class="exit"></span>\
+                            <div class="waves-effect waves-block effect jazz">\
+                                <div class="avatar"></div>\
+                                <div class="label">JAZZ</div>\
+                                <div class="volum">\
+                                    ' + comps.Slider( 0, 100, 50, "jazz" ) + '\
+                                </div>\
+                            </div>\
+                            <div class="waves-effect waves-block effect rain">\
+                                <div class="avatar"></div>\
+                                <div class="label">RAIN</div>\
+                                <div class="volum">\
+                                    ' + comps.Slider( 0, 100, 50, "rain" ) + '\
+                                </div>\
+                            </div>\
+                            <div class="waves-effect waves-block effect wind">\
+                                <div class="avatar"></div>\
+                                <div class="label">WIND</div>\
+                                <div class="volum">\
+                                    ' + comps.Slider( 0, 100, 50, "wind" ) + '\
+                                </div>\
+                            </div>\
                         </div>\
                     </div>';
         $( "body" ).append( tmpl );
@@ -75,6 +99,21 @@ define([ "jquery", "mousetrap", "lodash", "notify", "i18n", "comps", "message" ]
         });
         $( ".noise-mode .md-slider-root .volume" )[0].addEventListener( "slider", function( event ) {
             sounds[ "cafe001" ] && ( sounds[ "cafe001" ].volume = event.data / 100 );
+        });
+        $( ".noise-mode .scene" ).find( ".mode:last" ).on( "click", function( event ) {
+            $( ".noise-mode .sfx" ).css( "opacity", "1" ).css( "height", "100%" );
+        });
+        $( ".noise-mode .sfx .exit" ).on( "click", function( event ) {
+            $( ".noise-mode .sfx" ).css( "opacity", "0" ).css( "height", "0" );
+        });
+        $( ".md-slider-root .jazz" )[0].addEventListener( "slider", function( event ) {
+            // TO-DO
+        });
+        $( ".md-slider-root .rain" )[0].addEventListener( "slider", function( event ) {
+            // TO-DO
+        });
+        $( ".md-slider-root .wind" )[0].addEventListener( "slider", function( event ) {
+            // TO-DO
         });
     }
 
