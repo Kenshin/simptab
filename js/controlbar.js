27fa2293b94140f5b799ff5f89f240756035dd3e
Kenshin <fxblog@gmail.com>
2018-12-17 18:34:33 +0800

    Add earth mode background image position logic.

--- js/controlbar.js
+++ js/controlbar.js
@@ -21,14 +21,16 @@ define([ "jquery", "i18n", "vo", "date", "files", "setting", "manage", "about", 
         if ( value == "mask" ) {
             var url       = vo.cur.type == "default" ? vo.cur.hdurl : "filesystem:" + chrome.extension.getURL( "/" ) + "temporary/background.jpg",
                 maxHeight = 800,
-                height    = $( "body" ).height();
+                height    = $( "body" ).height(),
+                earth     = vo.cur.type == "earth" ? "background-size: contain;background-repeat: no-repeat;background-color: black;" : "";
             $( "body" ).addClass( "bgmask" ).prepend( '<div class="bgmask-bg"><img src="' + url + '"></div>' );
-            $( "head" ).append( '<style class="bgmask-filter">.bgmask::before{background: url(' + url + ')}</style>' );
+            $( "head" ).append( '<style class="bgmask-filter">.bgmask::before{background: url(' + url + ')' + earth + '}</style>' );
             height <= maxHeight && $( ".bgmask-bg" ).find( "img" ).height( height - 300 );
         } else {
             $( "body" ).removeClass( "bgmask" ).find( ".bgmask-bg" ).remove();
             $( ".bgmask-filter" ).remove();
             vo.cur.type == "default" || !value || value == "center" ? $( "body" ).addClass( "bgcenter" ) : $( "body" ).removeClass( "bgcenter" );
+            vo.cur.type == "earth" && $( "body" ).addClass( "bgearth" );
         }
     }
 
