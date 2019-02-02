b69b2f3314115606f846128d1d1df70a897303b4
Kenshin <fxblog@gmail.com>
2019-02-02 11:56:04 +0800

    Change send mobile shortcuts to 'e'.

--- js/shortcuts.js
+++ js/shortcuts.js
@@ -22,7 +22,7 @@ define([ "jquery", "mousetrap", "controlbar", "i18n", "topsites", "message" ], f
             { short: "t", long: "about"    },
             { short: "s", long: "setting"  },
             { short: "d", long: "desktop"  },
-            { short: "b", long: "mobile"   },
+            { short: "e", long: "mobile"   },
             { short: "a", long: "favorite" },
             { short: "n", long: "pin"      },
             { short: "u", long: "dislike"  },
