140ffb4e31ad39688e30c21d3f216d5a3c873a75
Kenshin <fxblog@gmail.com>
2019-11-19 11:29:52 +0800

    Add some guide message.

--- js/guide.js
+++ js/guide.js
@@ -6,27 +6,31 @@ define([ "jquery", "mousetrap", "lodash", "notify", "i18n" ], function( $, Mouse
     var intros,
         steps = [
             {
-                intro: '首次安装（或升级） 简悦会 <b>自动弹出</b> 设置界面，本节只针对设置界面最基本的操作。'
+                intro: '简 Tab 是一个纯粹、极简的新标签页扩展。<br>它的设计哲学: <b>只呈现美图，无任何干扰</b>，但这并以意味它仅代表简单，它的功能包含：<br><br>- <a href="" target="_blank">多种背景源</a>；<br>- <a href="" target="_blank">常用网址</a>；<br>- <a href="" target="_blank">书签栏</a>；<br>- <a href="" target="_blank">快捷搜索栏</a>；<br>- <a href="" target="_blank">白噪音</a>；<br>- <a href="" target="_blank">禅模式</a>；'
             },
             {
                 element: $( '.controlbar' )[0],
-                intro: '控制栏'
+                intro: '控制栏：包含了对背景图的大部分操作，如：上传、下载、更换背景源、更改背景模式等等；'
             },
             {
                 element: $( ".controlbar" ).find( "li" )[0],
-                intro: '控制栏 系统信息'
+                intro: '控制栏 - 系统信息，包含了：打开默认便签页、历史记录、下载地址、应用三个子项；'
             },
             {
                 element: $( ".controlbar" ).find( "li > a[url=fullscreen]" ).parent()[0],
-                intro: '控制栏 全屏'
+                intro: '控制栏 - 全屏：可将新标签页全屏、配合 <a href="" target="_blank">自动更换背景</a> 与 <a href="" target="_blank">白噪音</a>，可获得如同音乐相册的体验；'
             },
             {
                 element: $( ".controlbar" ).find( "li > a[url=info]" ).parent()[0],
-                intro: '控制栏 背景源信息'
+                intro: '控制栏 - 背景源信息：某些背景源具有源地址，如： Google Art / SimpTab 订阅源等，可查看这个美图背后的故事。'
             },
             {
-                element: $( ".controlbar" ).find( "li > a[url=info]" ).parent()[0],
-                intro: '控制栏 背景源信息'
+                element: $( ".controlbar" ).find( "li > a[url=download]" ).parent()[0],
+                intro: '控制栏 - 下载：可将背景下载，并可 <a href="" target="_blank">指定位置</a>。'
+            },
+            {
+                element: $( ".controlbar" ).find( "li > a[url=download]" ).parent()[0],
+                intro: '控制栏 - 下载：可将背景下载，并可 <a href="" target="_blank">指定位置</a>。'
             }
         ];
 
@@ -58,6 +62,9 @@ define([ "jquery", "mousetrap", "lodash", "notify", "i18n" ], function( $, Mouse
             } else if ( id == "controlbar-info" ) {
                 //$( $( ".controlbar" ).find( "a" )[3] ).trigger( "mouseenter" );
             }
+        });
+        intros.onexit( function() {
+            $( ".controlbar" ).removeAttr( "style" );
         });
         /*
         intros.onchange( function( target ) {
