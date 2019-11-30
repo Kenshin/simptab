dcc1b292d5ac96292d1d3afb1e3ffe9bd2151efa
Kenshin <fxblog@gmail.com>
2019-11-30 15:46:22 +0800

    Add intro to main css.

--- js/main.js
+++ js/main.js
@@ -95,10 +95,49 @@ $( document ).ready( function() {
         window.location.href = lng == ".en" ? "http://github.com/kenshin/simptab/blob/master/README" + lng + ".md" : "http://ksria.com/simptab/docs/#/";
     });
 
+    $( ".feature .learnmore").on( "click", function( event ) {
+        var id = $( event.currentTarget ).parent().parent().attr( "data-type" );
+        tooltips( id );
+    });
+
 });
 
 function getQueryString( name ) {
     var reg = new RegExp( "(^|&)" + name + "=([^&]*)(&|$)", "i" );
     var r = window.location.search.substr(1).match( reg );
     if (r !== null) return unescape(r[2]); return null;
+}
+
+function tooltips( id ) {
+    var steps  = {
+            origins: [{
+                element: $( ".feature[data-type=origins]" )[0],
+                intro: '简 Tab 支持从多个壁纸源获取数据，包括：<ul><li>必应每日图片</li><li>必应随机图片</li><li>wallhaven.cc</li><li>unsplash.com</li><li>googleartproject.com</li><li>desktoppr.co</li><li>visualhunt.com</li></ul>详细请看开发文档 <a href="https://simptab.art/docs/#/%E8%83%8C%E6%99%AF%E6%BA%90?id=%e5%a4%9a%e7%a7%8d%e8%83%8c%e6%99%af%e6%ba%90" target="_blank">多种背景源</a>'
+            }],
+            positionmode: [{
+                element: $( ".feature[data-type=positionmode]" )[0],
+                position: 'right',
+                intro: '简 Tab 具有多种方式的壁纸更换模式，包括：<ul><li>每天更换背景<br><abbr>每天更换一次，背景源只来源于 必应每日图片</abbr></li><li>随机更换<br><abbr>每次新打开标签页，即可看到一副新的背景图</abbr></li><li>只显示当前背景</li><li>地球每刻</li></ul>'
+            },
+            {
+                element: $( ".feature[data-type=positionmode]" )[0],
+                position: 'right',
+                intro: '<img src="./assets/image/earth.png" alt="地球每刻"><br><span style="border-left:.25em solid rgba(255,255,255,0.9);padding-left:5px;">地球每刻由 <a href="http://himawari8.nict.go.jp/" target="_blank">向日葵-8號</a> 提供，详细请看 <a href="https://simptab.art/docs/#/%E8%83%8C%E6%99%AF%E6%BA%90?id=%e5%9c%b0%e7%90%83%e6%af%8f%e5%88%bb" target="_blank">地球每刻</a></span>'
+            }]
+        },
+        intros = introJs();
+    intros.setOptions({
+        hintButtonLabel: "确认",
+        nextLabel: "下一条 →",
+        prevLabel: "← 上一条",
+        skipLabel: "",
+        doneLabel: "完成",
+        hidePrev: true,
+        hideNext: true,
+        exitOnEsc: true,
+        exitOnOverlayClick: true,
+        overlayOpacity: 0.2,
+        steps: steps[id],
+    });
+    intros.start();
 }
