ccfb382eb939fbeb8567581cd598387775201529
Kenshin <fxblog@gmail.com>
2019-12-03 15:35:00 +0800

    Remove introjs libs.

--- js/main.js
+++ js/main.js
@@ -117,56 +117,6 @@ function getQueryString( name ) {
     if (r !== null) return unescape(r[2]); return null;
 }
 
-/*
-function tooltips( id ) {
-    var steps  = {
-            origins: [{
-                element: $( ".feature[data-type=origins]" )[0],
-                position: 'right',
-                intro: '简 Tab 支持从多个壁纸源获取数据，包括：<ul><li>必应每日图片</li><li>必应随机图片</li><li>wallhaven.cc</li><li>unsplash.com</li><li>googleartproject.com</li><li>desktoppr.co</li><li>visualhunt.com</li></ul>详细请看开发文档 <a href="https://simptab.art/docs/#/%E8%83%8C%E6%99%AF%E6%BA%90?id=%e5%a4%9a%e7%a7%8d%e8%83%8c%e6%99%af%e6%ba%90" target="_blank">多种背景源</a>'
-            }],
-            positionmode: [{
-                element: $( ".feature[data-type=positionmode]" )[0],
-                position: 'right',
-                intro: '简 Tab 具有多种方式的壁纸更换模式，包括：<ul><li>每天更换背景<br><abbr>每天更换一次，背景源只来源于 必应每日图片</abbr></li><li>随机更换<br><abbr>每次新打开标签页，即可看到一副新的背景图</abbr></li><li>只显示当前背景</li><li>地球每刻</li></ul>详细请看开发文档 <a href="https://simptab.art/docs/#/%E8%83%8C%E6%99%AF%E6%BA%90?id=%e5%a4%9a%e7%a7%8d%e8%83%8c%e6%99%af%e6%ba%90" target="_blank">背景更换模式</a>'
-            },
-            {
-                element: $( ".feature[data-type=positionmode]" )[0],
-                position: 'right',
-                intro: '<img src="https://simptab-1254315611.cos.ap-shanghai.myqcloud.com/www/earth.jpg" alt="地球每刻"><br><span style="border-left:.25em solid rgba(255,255,255,0.9);padding-left:5px;">地球每刻由 <a href="http://himawari8.nict.go.jp/" target="_blank">向日葵-8號</a> 提供，详细请看 <a href="https://simptab.art/docs/#/%E8%83%8C%E6%99%AF%E6%BA%90?id=%e5%9c%b0%e7%90%83%e6%af%8f%e5%88%bb" target="_blank">地球每刻</a></span>'
-            }],
-            controlbar: [{
-                element: $( ".feature[data-type=controlbar]" )[0],
-                position: 'left',
-                intro: '直观的控制栏，包含多种操作方案：<br><ul><li>上传 · 下载</li><li>收藏</li><li>固定</li><li>下一张（刷新）</li><li>不喜欢</li></ul>详细请看开发文档 <a href="https://simptab.art/docs/#/%E6%8E%A7%E5%88%B6%E6%A0%8F" target="_blank">控制栏</a>'
-            }],
-            unsplash: [{
-                element: $( ".feature[data-type=unsplash]" )[0],
-                position: 'left',
-                intro: '深度集成 对 Unsplash 源的定制化设置，包含：<ul><li>自定义 Unsplash 源</li><li>更改 Unsplash 源分辨率</li></ul>'
-            }]
-        },
-        intros = introJs();
-    intros.setOptions({
-        hintButtonLabel: "确认",
-        nextLabel: "下一条 →",
-        prevLabel: "← 上一条",
-        skipLabel: "",
-        doneLabel: "完成",
-        hidePrev: true,
-        hideNext: true,
-        exitOnEsc: true,
-        exitOnOverlayClick: true,
-        overlayOpacity: 0.8,
-        steps: steps[id],
-    });
-    intros.onexit( function() {
-        $( ".feature" ).removeClass( "active" );
-    });
-    intros.start();
-}
-*/
-
 function tooltips( id ) {
     var $target = $( ".feature[data-type=" + id + "]" );
     $target.append( '<div class="more">' + i18nn( "feature_" + id ) + '</div>' );
